5bcf1a0c10188e6a258ef3359d8f18a671cc1a6f
Jash Patel <jashdetroj@gmail.com>
2020-06-18 09:44:43 +0530

    profile details half done

--- handlers/user.js
+++ handlers/user.js
@@ -1,8 +1,21 @@
 const db = require('../models')
 
-exports.test = async (req, res, next) => {
+// to copy paste while you create new functions
+exports.myprofile = async (req, res, next) => {
     try {
-        res.json({user: 'jash'})
+        
+        var { userId }= req.body
+
+        var user = await db.User.findById(userId)
+
+        if (user) {
+            console.log(user);
+            
+            res.json({success: true, msg: 'user found', profile: user})
+        } else {
+            res.json({success: false, msg: 'no such user found'})
+        }
+
     } catch (error) {
         error.status = 400
         console.log(error);        
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var handle = require('../handlers')
 
-router.get('/test', handle.test)
+router.post('/myprofile', handle.myprofile)
 
 module.exports = router
